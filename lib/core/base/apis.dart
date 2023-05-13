4fba053d212be2cbd214991d11e0e6e79734b627
ceshi <ceshi20160607@sina.com>
2023-05-13 17:24:07 +0800

    0.0.00001基础框架 0.4 修正

--- lib/core/base/apis.dart
+++ lib/core/base/apis.dart
@@ -1,3 +1,10 @@
 class APIS {
   static const login = "/user/doLogin";
+
+  ///noteCar
+  static const noteCarAdd = "/noteCar/add";
+  static const noteCarUpdate = "/noteCar/update";
+  static const noteCarQueryList = "/noteCar/queryList";
+  static const noteCarQueryById = "/noteCar/queryById";
+  static const noteCarDeleteById = "/noteCar/deleteById";
 }
